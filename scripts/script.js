e259c0754a973df2c7dd3d163eb151979e6b9565
spleennooname <spleen666@gmail.com>
2019-06-02 15:40:14 +0200

    fix typos

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-// spark-ar pixelate by scriting - Andrea Bovo <spleen666@gmail.com>
+// spark-ar script-only pixelate shader - Andrea Bovo <spleen666@gmail.com>
 
 // requires
-const Diagnostics = (D = console = require('Diagnostics'));
+const Diagnostics = D = console = require('Diagnostics');
 const Materials = require('Materials');
 const Textures = require('Textures');
 const Patches = require('Patches');
@@ -17,24 +17,19 @@ const cameraColor = Textures.get('cameraTex').signal;
 
 // get per-fragment uv
 const uvCoords = Shaders.fragmentStage(Shaders.vertexAttribute({ variableName: Shaders.VertexAttribute.TEX_COORDS }));
+// get screenSize via Patch
 const screenSize = Patches.getPoint2DValue('screenSize');
 
-// vars
+// get light intensity
 const lightIntensity = R.sub(1, LightingEstimation.frameBrightness);
+
 const res = R.pack2(screenSize.x, screenSize.y);
 const pixelSize = R.pack2(30, 30);
 
-let color = R.pack4(0, 0, 0, 1);
-
-// 1
-//let x = R.add(R.div(R.floor(R.mul(uv.x, px)), px), R.div(0.5, px));
-//let y = R.add(R.div(R.floor(R.mul(uv.y, py)), py), R.div(0.5, py));
-
-// 2
-// let x = R.div(R.floor(R.mul(uvCoords.x, px)), px);
-// let y = R.div(R.floor(R.mul(uvCoords.y, py)), py) */;
 const tileX = R.div(pixelSize.x, res.x);
 const tileY = R.div(pixelSize.y, res.y);
+
+let color = R.pack4(0, 0, 0, 1);
 let x = R.mul(tileX, R.floor(R.div(uvCoords.x, tileX)));
 let y = R.mul(tileY, R.floor(R.div(uvCoords.y, tileY)));
 
