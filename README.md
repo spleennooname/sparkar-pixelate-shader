861451d253f681b51cabc2e062431a329df8da32
spleennooname <spleen666@gmail.com>
2019-06-19 17:14:17 +0200

    add preview && update script

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 > simple script-only pixelate shader with Facebook SparkAR.
 
+![preview](https://spleennooname.github.io/sparkar-pixelate-shader/preview.jpg)
 
 ## credits
 
-* thx to [Kuno Hayato](https://github.com/kunofellasleep) for for showing me the way with [this article](https://medium.com/birdman-inc/gaussian-blur-on-spark-ar-studio-833e171160d5).
+* thxx to [Kuno Hayato](https://github.com/kunofellasleep) for for showing me the way with [this article](https://medium.com/birdman-inc/gaussian-blur-on-spark-ar-studio-833e171160d5).
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 // spark-ar script-only pixelate shader - Andrea Bovo <spleen666@gmail.com>
 
 // requires
-const Diagnostics = D = console = require('Diagnostics');
 const Materials = require('Materials');
 const Textures = require('Textures');
 const Patches = require('Patches');
@@ -10,24 +9,27 @@ const R = require('Reactive');
 
 // settings
 const defaultMaterial0 = Materials.get('defaultMaterial0');
+// get camera shader signal
 const cameraColor = Textures.get('cameraTex').signal;
 // get per-fragment uv
 const uvCoords = Shaders.fragmentStage(Shaders.vertexAttribute({ variableName: Shaders.VertexAttribute.TEX_COORDS }));
-// get screenSize via Patch
+// get screenSize from patch
 const screenSize = Patches.getPoint2DValue('screenSize');
-
+// get resolution
 const res = R.pack2(screenSize.x, screenSize.y);
-const pixelSize = R.pack2(30, 30);
 
+// pixelate part
+const pixelSize = R.pack2(30, 30);
 const tileX = R.div(pixelSize.x, res.x);
 const tileY = R.div(pixelSize.y, res.y);
 
-const uvPixelated = R.pack2(
-  R.mul(tileX, R.floor(R.div(uvCoords.x, tileX))),
-  R.mul(tileY, R.floor(R.div(uvCoords.y, tileY)))
+// create pixelated uv
+const newUV = R.pack2(
+  tileX.mul(R.floor(R.div(uvCoords.x, tileX))),
+  tileY.mul(R.floor(R.div(uvCoords.y, tileY)))
 );
 
 // sampling texture color at uv coords
-let color = Shaders.textureSampler(cameraColor, uvPixelated);
+const color = Shaders.textureSampler(cameraColor, newUV);
 // Assign the shader signal to the texture slot
 defaultMaterial0.setTexture(color, { textureSlotName: Shaders.DefaultMaterialTextures.DIFFUSE });
